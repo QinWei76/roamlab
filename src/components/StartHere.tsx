47131dfec1b6c0a28e034c945fe83add2c3bd6da
QinWei76 <18618121169@163.com>
2026-08-25 15:01:00 +0800

    Refactor StartHerePage component imports and structure

--- src/components/StartHere.tsx
+++ src/components/StartHere.tsx
@@ -7,23 +7,39 @@ import CompleteSystem from "@/components/CompleteSystem";
 export default function StartHerePage() {
 
   return (
-
     <main>
 
       <StartHere />
 
-
       <FirstSetup />
-
 
       <UpgradeGear />
 
+      <CompleteSystem />
+
+    </main>
+  );
+
+}import StartHere from "@/components/StartHere";
+import FirstSetup from "@/components/FirstSetup";
+import UpgradeGear from "@/components/UpgradeGear";
+import CompleteSystem from "@/components/CompleteSystem";
+
+
+export default function StartHerePage() {
+
+  return (
+    <main>
+
+      <StartHere />
+
+      <FirstSetup />
+
+      <UpgradeGear />
 
       <CompleteSystem />
 
-
     </main>
-
   );
 
 }
