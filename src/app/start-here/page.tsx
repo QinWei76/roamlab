4f4d681c7dee54c2d53de6c560b613f58842a7fb
QinWei76 <18618121169@163.com>
2026-08-26 08:08:04 +0800

    Add CTA component to StartHerePage

--- src/app/start-here/page.tsx
+++ src/app/start-here/page.tsx
@@ -2,7 +2,27 @@ import StartHere from "@/components/StartHere";
 import FirstSetup from "@/components/FirstSetup";
 import UpgradeGear from "@/components/UpgradeGear";
 import CompleteSystem from "@/components/CompleteSystem";
+import CTA from "@/components/CTA";
 
-export default function StartHerePage(){
- return <main><StartHere/><FirstSetup/><UpgradeGear/><CompleteSystem/></main>
+
+export default function StartHerePage() {
+
+  return (
+
+    <main>
+
+      <StartHere />
+
+      <FirstSetup />
+
+      <UpgradeGear />
+
+      <CompleteSystem />
+
+      <CTA />
+
+    </main>
+
+  );
+
 }
