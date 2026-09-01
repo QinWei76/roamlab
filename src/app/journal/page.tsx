2b11a926324e6bb9dde0901da6148a5668b516b3
QinWei76 <18618121169@163.com>
2026-09-01 14:28:41 +0800

    Create JournalPage component for journaling

---
+++ src/app/journal/page.tsx
@@ -0,0 +1,26 @@
+export default function JournalPage(){
+
+return (
+
+<main className="system-page">
+
+<section className="system-card">
+
+
+<h1>
+MY WILD JOURNAL
+</h1>
+
+
+<p>
+Record your adventures.
+</p>
+
+
+</section>
+
+</main>
+
+)
+
+}
